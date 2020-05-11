f7ab95f2a90d9ee932846d2e7ed5192d4995697d
henriqueeyer <henriqueeyer@outlook.com>
2020-05-11 10:36:37 -0300

    Teste criado para verificar existencia da label e do input da tarefa

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,5 +3,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('Verificando um se existe um campo para inserir a tabela.', () => {
-
+  const { getByLabelText, getByText } = render(<App />)
+  expect(getByText('Tarefa:')).toBeInTheDocument();
+  expect(getByLabelText('Tarefa:')).toBeInTheDocument();
 });
