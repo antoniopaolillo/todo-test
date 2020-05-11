b2510767794531d611cd69a72e322f6d556a39f9
henriqueeyer <henriqueeyer@outlook.com>
2020-05-11 10:47:56 -0300

    Corrigindo a descriçao do test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('Verificando um se existe um campo para inserir a tabela.', () => {
+test('Verificando se existe um campo para inserir a tarefa.', () => {
   const { getByLabelText, getByText } = render(<App />)
   expect(getByText('Tarefa:')).toBeInTheDocument();
   expect(getByLabelText('Tarefa:')).toBeInTheDocument();
