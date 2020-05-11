954662cde3bee2cf2944fb85c0281879cbbda216
henriqueeyer <henriqueeyer@outlook.com>
2020-05-11 10:24:38 -0300

    Criando arquivo e descriçao do teste

---
+++ test/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Verificando um se existe um campo para inserir a tabela.', () => {
+
+});
