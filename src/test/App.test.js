e11654ab64a39837096d78460f635933b1f0b403
henriqueeyer <henriqueeyer@outlook.com>
2020-05-11 11:00:12 -0300

    Corrigindo os nomes, trocando email para task

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,14 +4,14 @@ import App from '../App';
 
 describe('Testando a aplicação, testando input', () => {
   const { getByLabelText, getByText } = render(<App />)
-  const inputEmail = getByLabelText('Tarefa:');
-  const labelEmail = getByText('Tarefa:');
+  const inputTask = getByLabelText('Tarefa:');
+  const labelTask = getByText('Tarefa:');
   test('Verificando se o label e o input existem no documento', () => {
-    expect(labelEmail).toBeInTheDocument();
-    expect(inputEmail).toBeInTheDocument();
+    expect(labelTask).toBeInTheDocument();
+    expect(inputTask).toBeInTheDocument();
   });
     
   test('Verificando o tipo do input', () => {
-    expect(inputEmail.type).toBe('text');
+    expect(inputTask.type).toBe('text');
   });
 });
