2851f5b15e73f6a8bea0c4f5834e8af6f5a8e27b
henriqueeyer <henriqueeyer@outlook.com>
2020-05-11 18:44:23 -0300

    Adicionando as descriçoes/orientaçoes para o exercicio 1

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,15 +3,30 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('Testando a aplicação, testando input', () => {
-  const { getByLabelText, getByText } = render(<App />)
+  const { getByLabelText, getByText } = render(<App />);
   const inputTask = getByLabelText('Tarefa:');
   const labelTask = getByText('Tarefa:');
+
   test('Verificando se o label e o input existem no documento', () => {
     expect(labelTask).toBeInTheDocument();
     expect(inputTask).toBeInTheDocument();
   });
-    
+
   test('Verificando o tipo do input', () => {
     expect(inputTask.type).toBe('text');
+  });
+});
+
+describe('Testando a aplicação, testando botão, e sua funcionalidade', () => {
+
+  test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
+
+  });
+
+  test(`
+    Verificando sua funcionalidade, ao ser clicado é necessário adicionar o que o usuário digitou
+  Use os fireEvent, para simular a digitação do usuário e o clique.
+  `, () => {
+
   });
 });
